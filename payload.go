c4cd391894c204ab50c3b260257605a65a315ba6
Karl Kirch <kkirch@wdtinc.com>
2014-07-10 17:03:22 -0500

    Add payload code
    Payload struct and marshalling + tests

---
+++ payload.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+    "encoding/json"
+    "errors"
+    "fmt"
+)
+
+type Payload struct {
+    AlertText            string
+    Badge                int
+    Sound                string
+    ContentAvailable     int
+    ActionLocKey         string
+    LocKey               string
+    LocArgs              []string
+    LaunchImage          string
+    CustomFields         map[string]interface{}
+}
+
+type apsAlertBody struct {
+    Body                 string     `json:"body,omitempty"`
+    ActionLocKey         string     `json:"action-loc-key,omitempty"`
+    LocKey               string     `json:"loc-key,omitempty"`
+    LocArgs              []string   `json:"loc-args,omitempty"`
+    LaunchImage          string     `json:"launch-image,omitempty"`
+}
+
+type alertBodyAps struct {
+    Alert                apsAlertBody `json:"alert,omitempty"`
+	Badge                int          `json:"badge,omitempty"`
+	Sound                string       `json:"sound,omitempty"`
+    ContentAvailable     int          `json:"content-available,omitempty"`
+}
+
+type simpleAps struct {
+    Alert                string     `json:"alert,omitempty"`
+    Badge                int        `json:"badge,omitempty"`
+    Sound                string     `json:"sound,omitempty"`
+    ContentAvailable     int        `json:"content-available,omitempty"`
+}
+
+func (p *Payload) Marshal(maxPayloadSize int) (string, error) {
+    // found in https://github.com/anachronistic/apns/blob/master/push_notification.go#L93
+    // This deserves some explanation.
+	//
+	// Setting an exported field of type int to 0
+	// triggers the omitempty behavior if you've set it.
+	// Since the badge is optional, we should omit it if
+	// it's not set. However, we want to include it if the
+	// value is 0, so there's a hack in payload.go
+	// that exploits the fact that Apple treats -1 for a
+	// badge value as though it were 0 (i.e. it clears the
+	// badge but doesn't stop the notification from going
+	// through successfully.)
+	//
+	// Still a hack though :)
+	if p.Badge == 0 {
+		p.Badge = -1
+	}
+    if p.isSimple() {
+        return p.marshalSimplePayload(maxPayloadSize)
+    } else {
+        return p.marshalAlertBodyPayload(maxPayloadSize)
+    }
+}
+
+func (p *Payload) isSimple() bool {
+    return p.ActionLocKey == "" && p.LocKey == "" &&
+           p.LocArgs == nil && p.LaunchImage == ""
+}
+
+func constructFullPayload(aps interface{}, customFields map[string]interface{}) (map[string]interface{}, error) {
+    var fullPayload map[string]interface{} = make(map[string]interface{})
+    fullPayload["aps"] = aps
+    for key, value := range customFields {
+        if key == "aps" {
+            return nil, errors.New("Cannot have a custom field named aps")
+        }
+        fullPayload[key] = value
+    }
+    return fullPayload, nil
+}
+
+func (p *Payload) marshalSimplePayload(maxPayloadSize int) (string, error) {
+    var jsonStr []byte = nil
+
+    //use simple payload
+    aps := simpleAps{
+        Alert: p.AlertText,
+        Badge: p.Badge,
+        Sound: p.Sound,
+        ContentAvailable: p.ContentAvailable,
+    }
+
+    fullPayload, err := constructFullPayload(aps, p.CustomFields)
+    if (err != nil) {
+        return "", err
+    }
+
+    jsonStr, err = json.Marshal(fullPayload)
+    if (err != nil) {
+        return "", err
+    }
+
+    payloadLen := len(jsonStr)
+
+    if payloadLen > maxPayloadSize {
+        clipSize := payloadLen - (maxPayloadSize) + 3 //need extra characters for ellipse
+        if clipSize > len(p.AlertText) {
+            return "", errors.New(fmt.Sprintf("Payload was too long to successfully marshall to less than %v", maxPayloadSize))
+        }
+        aps.Alert = aps.Alert[:len(aps.Alert) - clipSize] + "..."
+        fullPayload["aps"] = aps
+        if (err != nil) {
+            return "", err
+        }
+
+        jsonStr, err = json.Marshal(fullPayload)
+        if (err != nil) {
+            return "", err
+        }
+    }
+
+    return string(jsonStr), nil
+}
+
+func (p *Payload) marshalAlertBodyPayload(maxPayloadSize int) (string, error) {
+    var jsonStr []byte = nil
+
+    //use alertBody payload
+    alertBody := apsAlertBody{
+        Body: p.AlertText,
+        ActionLocKey: p.ActionLocKey,
+        LocKey: p.LocKey,
+        LocArgs: p.LocArgs,
+        LaunchImage: p.LaunchImage,
+    }
+
+    aps := alertBodyAps{
+        Alert: alertBody,
+        Badge: p.Badge,
+        Sound: p.Sound,
+        ContentAvailable: p.ContentAvailable,
+    }
+
+    fullPayload, err := constructFullPayload(aps, p.CustomFields)
+    if (err != nil) {
+        return "", err
+    }
+
+    jsonStr, err = json.Marshal(fullPayload)
+    if (err != nil) {
+        return "", err
+    }
+
+    payloadLen := len(jsonStr)
+
+    if payloadLen > maxPayloadSize {
+        clipSize := payloadLen - (maxPayloadSize) + 3 //need extra characters for ellipse
+        if clipSize > len(p.AlertText) {
+            return "", errors.New(fmt.Sprintf("Payload was too long to successfully marshall to less than %v", maxPayloadSize))
+        }
+        aps.Alert.Body = aps.Alert.Body[:len(aps.Alert.Body) - clipSize] + "..."
+        fullPayload["aps"] = aps
+        if (err != nil) {
+            return "", err
+        }
+
+        jsonStr, err = json.Marshal(fullPayload)
+        if (err != nil) {
+            return "", err
+        }
+    }
+
+    return string(jsonStr), nil
+}
---
+++ payload_test.go
@@ -0,0 +1,474 @@
+package main
+
+import (
+    "fmt"
+    "testing"
+)
+
+func TestSimpleMarshal(t *testing.T) {
+    p := Payload{
+        AlertText: "Testing this payload",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+    }
+
+    payloadSize := 256
+
+    json, err := p.Marshal(payloadSize)
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(json) > payloadSize {
+        t.Error(fmt.Sprintf("Expected payload to be less than %v but was %v", payloadSize, len(json)))
+    }
+
+    expectedJson := "{\"aps\":{\"alert\":\"Testing this payload\",\"badge\":2,\"sound\":\"test.aiff\",\"content-available\":1}}"
+    if json != expectedJson {
+        t.Error(fmt.Sprintf("Expected %v but got %v", expectedJson, json))
+    }
+}
+
+func TestSimpleMarshalWithCustomFields(t *testing.T) {
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "obj": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+    }
+
+    payloadSize := 256
+
+    json, err := p.Marshal(payloadSize)
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(json) > payloadSize {
+        t.Error(fmt.Sprintf("Expected payload to be less than %v but was %v", payloadSize, len(json)))
+    }
+
+    expectedJson := "{\"aps\":{\"alert\":\"Testing this payload\",\"badge\":2,\"sound\":\"test.aiff\",\"content-available\":1},\"arr\":[\"a\",2],\"num\":55,\"obj\":{\"obja\":\"a\",\"objb\":\"b\"},\"str\":\"string\"}"
+    if json != expectedJson {
+        t.Error(fmt.Sprintf("Expected %v but got %v", expectedJson, json))
+    }
+}
+
+func TestSimpleMarshalTruncate(t *testing.T) {
+    p := Payload{
+        AlertText: "Testing this payload with a really long message that should " +
+            "cause the payload to be truncated yay and stuff blah blah blah blah blah blah " +
+            "and some more text to really make this much bigger and stuff",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+    }
+
+    payloadSize := 256
+
+    json, err := p.Marshal(payloadSize)
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(json) > payloadSize {
+        t.Error(fmt.Sprintf("Expected payload to be less than %v but was %v", payloadSize, len(json)))
+    }
+
+    expectedJson := "{\"aps\":{\"alert\":\"Testing this payload with a really long message that should cause the payload to be truncated yay and stuff blah blah blah blah blah blah and some more text to really make this much...\",\"badge\":2,\"sound\":\"test.aiff\",\"content-available\":1}}"
+    if json != expectedJson {
+        t.Error(fmt.Sprintf("Expected %v but got %v", expectedJson, json))
+    }
+}
+
+func TestSimpleMarshalTruncateWithCustomFields(t *testing.T) {
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "obj": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload with a bunch of text that should get truncated " +
+                    "so truncate this already please yes thank you blah blah blah blah blah blah " +
+                    "plus some more text",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+    }
+
+    payloadSize := 256
+
+    json, err := p.Marshal(payloadSize)
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(json) > payloadSize {
+        t.Error(fmt.Sprintf("Expected payload to be less than %v but was %v", payloadSize, len(json)))
+    }
+
+    expectedJson := "{\"aps\":{\"alert\":\"Testing this payload with a bunch of text that should get truncated " +
+                    "so truncate this already please yes thank you...\",\"badge\":2,\"sound\":\"test.aiff\",\"content-available\":1},"+
+                    "\"arr\":[\"a\",2],\"num\":55,\"obj\":{\"obja\":\"a\",\"objb\":\"b\"},\"str\":\"string\"}"
+    if json != expectedJson {
+        t.Error(fmt.Sprintf("Expected %v but got %v", expectedJson, json))
+    }
+}
+
+func TestSimpleMarshalThrowErrorIfPayloadTooBigWithCustomFields(t *testing.T) {
+    //lots of custom fields to force failure
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "obj": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+        "obj2": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+        "obj3": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+        "obj4": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+        "obj5": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+    }
+
+    payloadSize := 256
+
+    _, err := p.Marshal(payloadSize)
+    if err == nil {
+        t.Error("Should have thrown marshaling error")
+    }
+}
+
+func TestAlertBodyMarshal(t *testing.T) {
+    p := Payload{
+        AlertText: "Testing this payload",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        ActionLocKey: "act-loc-key",
+        LocKey: "loc-key",
+        LocArgs: []string{"arg1", "arg2"},
+        LaunchImage: "launch.png",
+    }
+
+    payloadSize := 256
+
+    json, err := p.Marshal(payloadSize)
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(json) > payloadSize {
+        t.Error(fmt.Sprintf("Expected payload to be less than %v but was %v", payloadSize, len(json)))
+    }
+
+    expectedJson := "{\"aps\":{\"alert\":{\"body\":\"Testing this payload\",\"action-loc-key\":\"act-loc-key\",\"loc-key\":\"loc-key\",\"loc-args\":[\"arg1\",\"arg2\"],\"launch-image\":\"launch.png\"},\"badge\":2,\"sound\":\"test.aiff\",\"content-available\":1}}"
+    if json != expectedJson {
+        t.Error(fmt.Sprintf("Expected %v but got %v", expectedJson, json))
+    }
+}
+
+func TestAlertBodyMarshalWithCustomFields(t *testing.T) {
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "obj": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+        ActionLocKey: "act-loc-key",
+        LocKey: "loc-key",
+        LaunchImage: "launch.png",
+    }
+
+    payloadSize := 256
+
+    json, err := p.Marshal(payloadSize)
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(json) > payloadSize {
+        t.Error(fmt.Sprintf("Expected payload to be less than %v but was %v", payloadSize, len(json)))
+    }
+
+    expectedJson := "{\"aps\":{\"alert\":{\"body\":\"Testing this payload\",\"action-loc-key\":\"act-loc-key\",\"loc-key\":\"loc-key\"," +
+        "\"launch-image\":\"launch.png\"}," +
+        "\"badge\":2,\"sound\":\"test.aiff\",\"content-available\":1},\"arr\":[\"a\",2]," +
+        "\"num\":55,\"obj\":{\"obja\":\"a\",\"objb\":\"b\"},\"str\":\"string\"}"
+
+    if json != expectedJson {
+        t.Error(fmt.Sprintf("Expected %v but got %v", expectedJson, json))
+    }
+}
+
+func TestAlertBodyMarshalTruncate(t *testing.T) {
+    p := Payload{
+        AlertText: "Testing this payload with a really long message that should " +
+            "cause the payload to be truncated yay and stuff blah blah blah blah blah blah " +
+            "and some more text to really make this much bigger and stuff",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        LaunchImage: "launch.png",
+    }
+
+    payloadSize := 256
+
+    json, err := p.Marshal(payloadSize)
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(json) > payloadSize {
+        t.Error(fmt.Sprintf("Expected payload to be less than %v but was %v", payloadSize, len(json)))
+    }
+
+    expectedJson := "{\"aps\":{\"alert\":{\"body\":\"Testing this payload with a really long message that should cause the payload to be truncated yay and stuff blah blah blah blah blah blah and so...\",\"launch-image\":\"launch.png\"},\"badge\":2,\"sound\":\"test.aiff\",\"content-available\":1}}"
+    if json != expectedJson {
+        t.Error(fmt.Sprintf("Expected %v but got %v", expectedJson, json))
+    }
+}
+
+func TestAlertBodyMarshalTruncateWithCustomFields(t *testing.T) {
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "arr2": []interface{}{"a", 2},
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload with a bunch of text that should get truncated " +
+                    "so truncate this already please yes thank you blah blah blah blah blah blah " +
+                    "plus some more text",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+        ActionLocKey: "act-loc-key",
+        LocKey: "loc-key",
+        LocArgs: []string{"arg1", "arg2"},
+        LaunchImage: "launch.png",
+    }
+
+    payloadSize := 256
+
+    json, err := p.Marshal(payloadSize)
+    if err != nil {
+        t.Error(err)
+    }
+
+    if len(json) > payloadSize {
+        t.Error(fmt.Sprintf("Expected payload to be less than %v but was %v", payloadSize, len(json)))
+    }
+
+    expectedJson := "{\"aps\":{\"alert\":{\"body\":\"Testing this ...\",\"action-loc-key\":\"act-loc-key\",\"loc-key\":\"loc-key\"," +
+                    "\"loc-args\":[\"arg1\",\"arg2\"],\"launch-image\":\"launch.png\"},\"badge\":2,\"sound\":\"test.aiff\",\"content-available\":1},"+
+                    "\"arr\":[\"a\",2],\"arr2\":[\"a\",2],\"num\":55,\"str\":\"string\"}"
+    if json != expectedJson {
+        t.Error(fmt.Sprintf("Expected %v but got %v", expectedJson, json))
+    }
+}
+
+func TestAlertBodyMarshalThrowErrorIfPayloadTooBigWithCustomFields(t *testing.T) {
+    //lots of custom fields to force failure
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "obj": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+        "obj2": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+        "obj3": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+        "obj4": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+        "obj5": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+        LaunchImage: "launch.png",
+    }
+
+    payloadSize := 256
+
+    _, err := p.Marshal(payloadSize)
+    if err == nil {
+        t.Error("Should have thrown marshaling error")
+    }
+}
+
+func BenchmarkSimpleMarshalTruncate256WithCustomFields(b *testing.B) {
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "obj": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload with a bunch of text that should get truncated " +
+                    "so truncate this already please yes thank you blah blah blah blah blah blah " +
+                    "plus some more text",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+    }
+
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        p.Marshal(256)
+    }
+}
+
+func BenchmarkSimpleMarshalTruncate1024WithCustomFields(b *testing.B) {
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "obj": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload with a bunch of text that should get truncated " +
+                    "so truncate this already please yes thank you blah blah blah blah blah blah " +
+                    "plus some more text",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+    }
+
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        p.Marshal(1024)
+    }
+}
+
+func BenchmarkAlertBodyMarshalTruncate256WithCustomFields(b *testing.B) {
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "obj": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload with a bunch of text that should get truncated " +
+                    "so truncate this already please yes thank you blah blah blah blah blah blah " +
+                    "plus some more text",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+        LaunchImage: "launch.png",
+    }
+
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        p.Marshal(256)
+    }
+}
+
+func BenchmarkAlertBodyMarshalTruncate1024WithCustomFields(b *testing.B) {
+    customFields := map[string]interface{}{
+        "num": 55,
+        "str": "string",
+        "arr": []interface{}{"a", 2},
+        "obj": map[string]string {
+            "obja":"a",
+            "objb":"b",
+        },
+    }
+
+    p := Payload{
+        AlertText: "Testing this payload with a bunch of text that should get truncated " +
+                    "so truncate this already please yes thank you blah blah blah blah blah blah " +
+                    "plus some more text",
+        Badge: 2,
+        ContentAvailable: 1,
+        Sound: "test.aiff",
+        CustomFields: customFields,
+        LaunchImage: "launch.png",
+    }
+
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        p.Marshal(1024)
+    }
+}
